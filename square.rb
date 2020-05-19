1808cafd643423d84cffdf538b997ace671dca05
bocalo <moldavskiym@yandex.ru>
2020-05-19 15:02:04 +0300

    First tasks in ruby

---
+++ square.rb
@@ -0,0 +1,7 @@
+puts "Введите значение основания(a)"
+a = gets.chomp.to_i
+puts "Введите значение высоты треугольника(h)"
+h = gets.chomp.to_i
+square = 0.5 * a * h
+puts
+puts "Площадь треугольника: #{square.to_i}"
---
+++ weight.rb
@@ -0,0 +1,14 @@
+puts "Здравствуйте, как вас зовут?"
+name = gets.chomp
+puts "#{name}, какой у вас рост?"
+height = gets.chomp.to_i
+
+ideal_weight = (height - 110) * 1.15
+
+puts "#{name}, Ваш идеальный вес: #{ideal_weight}" 
+
+if ideal_weight < 0
+    puts  "Ваш вес уже оптимальный" 
+else
+    puts "Надо что-то делать"
+end
